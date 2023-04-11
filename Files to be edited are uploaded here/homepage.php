e68d4f7822292fbd2596c563f848420c4d0f36bb
Harsha K A <97173356+Harsha-02@users.noreply.github.com>
2023-04-11 22:16:17 +0530

    Create homepage.php

---
+++ Files to be edited are uploaded here/homepage.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<header class=".bg-light">
+        <div class="row">
+            <div class="col-md-12 my-auto">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    
+                    <a class="navbar-brand" href="loginpage.html">JYOTHI ENGINEERING COLLEGE</a>
+                  
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                          <a class="nav-link" href="#">E-Forum <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">About Us</a>
+                        </li>
+                       <!--- <li class="nav-item">
+                          <a class="nav-link disabled" href="#">Disabled</a>
+                        </li> --->
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">News</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Contact Us</a>
+                        </li>
+                        <li class="nav-item active Login">
+                          <a class="nav-link" href="loginpage.php">Login <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                          <a class="nav-link" href="createnewaccount.php">Register <span class="sr-only">(current)</span></a>
+                        </li>
+                        
+                        
+                      </ul>
+                     <!--- <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                      </form> --->
+                    </div>
+                  </nav>
+            </div>
+        </div>
+    </header>
+    <nav>
+    <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-interval="10000">
+      <img src="images/maxresdefault.jpg" class="d-block w-100 image" alt="Alumni Management System">
+      <div class="carousel-caption d-none d-md-block" >
+        <h1><marquee>Alumni Management System</marquee></h1>
+      </div>
+    </div>
+    <div class="carousel-item" data-interval="2000">
+    <div class="carousel-caption d-none d-md-block" >
+    <h1>Alumni Management System</h1>
+      </div>
+      <img src="images/canteen.png" class="d-block w-100 image" alt="Alumni Management System">
+    </div>
+    <div class="carousel-item">
+      <img src="images/download.jpg" class="d-block w-100 image" alt="Alumni Management System">
+      <div class="carousel-caption d-none d-md-block" >
+        <h1>Alumni Management System</h1>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<div>
+    <h2>DEPARTMENT OF COMPUTER SCIENCE & ENGINEERING
+</h2>
+</div>
+<div>
+    <h3>Vision of the Department</h3>
+    <p>
+    
+   <ul>
+    <li>
+    Creating eminent and ethical leaders in the domain of Computational Sciences through quality
+professional education with a focus on holistic learning and excellence.
+    </li>
+   </ul>
+</div>
+<div class="">
+    </p>
+    <h3>Mission of the Department</h3>
+    <p>
+        <ul>
+        <li>To create technically competent and ethically conscious graduates in the field of Computer
+         Science and Engineering by encouraging holistic learning and excellence.
+         </li>
+         <li>To prepare students for careers in Industry, Academia and the Government.
+         </li>
+         <li>
+         To instill Entrepreneurial Orientation and research motivation among the students of the
+         department.
+         </li>
+         <li>
+         To emerge as a leader in education in the region by encouraging teaching, learning, industry
+         and societal connect.
+         </li>
+         </ul>
+    </p>
+</div>
+
+
+<?php
+
+  
+?>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
