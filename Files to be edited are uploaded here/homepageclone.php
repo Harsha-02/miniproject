52e369b20f48b2d57ebda5718e1b7de8c76453d1
Harsha K A <97173356+Harsha-02@users.noreply.github.com>
2023-04-18 23:01:15 +0530

    Create homepageclone.php

---
+++ Files to be edited are uploaded here/homepageclone.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alumni Management System</title>
+    
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    
+
+
+
+<header class=" .bg-light">
+<div class="row">
+            <div class="col-md-12 my-auto">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">JYOTHI ENGINEERING COLLEGE</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="homepageclone.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="loginpage.php">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="createnewaccount.php">Register</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Link
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Link</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+           
+
+
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="maxresdefault.jpg" class="d-block w-100 image" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="download.jpg" class="d-block w-100 image" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="canteen.png" class="d-block w-100 image" alt="...">
+    </div>
+  </div>
+</div>
+
+</div>
+        </div>
+</header>
+
+
+<div>
+    <h2>DEPARTMENT OF COMPUTER SCIENCE & ENGINEERING
+</h2>
+</div>
+<div class="split left">
+<div>
+    <h3>Vision of the Department</h3>
+    <p>
+    
+   <ul>
+    <li>
+    Creating eminent and ethical leaders in the domain of Computational Sciences through quality
+professional education with a focus on holistic learning and excellence.
+    </li>
+   </ul>
+</div>
+</div>
+<div class="split right">
+<div>
+    </p>
+    <h3>Mission of the Department</h3>
+    <p>
+        <ul>
+        <li>To create technically competent and ethically conscious graduates in the field of Computer
+         Science and Engineering by encouraging holistic learning and excellence.
+         </li>
+         <li>To prepare students for careers in Industry, Academia and the Government.
+         </li>
+         <li>
+         To instill Entrepreneurial Orientation and research motivation among the students of the
+         department.
+         </li>
+         <li>
+         To emerge as a leader in education in the region by encouraging teaching, learning, industry
+         and societal connect.
+         </li>
+         </ul>
+    </p>
+</div>
+</div>
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
